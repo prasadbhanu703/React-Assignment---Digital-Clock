4751ed8edb23bf34165795ef9e9ac726f5c8b035
prasadbhanu703 <prasadbhanu703@gmail.com>
2020-12-09 19:38:39 +0530

    final check 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
 
+
 class App extends Component {
     constructor(props) {
         super(props);
@@ -17,11 +18,11 @@ class App extends Component {
         )
     }
     componentDidMount() {  
-        this.interval = setInterval( 
-            () => this.setState({
+        this.interval = setInterval(() => {
+                this.setState({
                 time: new Date()
-            }), 
-            1000); 
+            })
+         }, 1*1000); 
      }
     componentWillUnmount()  {
 
@@ -48,3 +49,59 @@ class App extends Component {
 }
 
 export default App;
+
+
+// class App extends Component {
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             time : new Date()
+//         };
+//         this.intervalId = null;
+//     }
+//     render() {
+
+//         return(
+//             <div className="Clock">
+//                <h3 id="time">{this.getTimeString()}</h3>
+//             </div>
+//         );
+//     }
+
+//     componentDidMount(){
+//         this.intervalId = setInterval(() => {
+//             this.setState({
+//                 time : new Date()
+//             })
+//         },1*1000);
+//     }
+//     componentWillUnmount(){
+//         clearInterval(this.intervalId);
+//     }
+    
+//     getTimeString() {
+//         const currTime = this.state.time;
+//         const [hours,minutes,seconds] = [
+//             currTime.getHours(),
+//             currTime.getMinutes(),
+//             currTime.getSeconds(),
+//         ];
+//         const amOrPm = hours >= 12 ? "PM" : "Am";
+//         const twelveHourFormat = hours > 12 ? hours - 12 : hours;
+//         const hourString = '' + twelveHourFormat;
+//         const minuteString = this.padNumberToTwoDigits(minutes);
+//         const secondString = this.padNumberToTwoDigits(seconds);
+
+//         const timeString = `${hourString}:${minuteString}:${secondString} ${amOrPm}`;
+
+//         return timeString;
+//     }
+//     padNumberToTwoDigits(num){
+//         return `${num < 10 ? "0" : ""}${num}`;
+//     }
+// }
+
+
+// export default App;
+
+
